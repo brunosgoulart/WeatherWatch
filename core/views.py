681745270d5f23f6248ced067cfc3de16eb119a3
bsg <77671906+brunosgoulart@users.noreply.github.com>
2024-07-03 20:36:21 -0300

    Graph's x-scale isn't working

--- core/views.py
+++ core/views.py
@@ -10,11 +10,9 @@ def home (request):
     return render (request, 'index.html', {'data': forecast_data})
 
 def chart (request):
-    timestamp_list = list()
-    temperature_list = list()
+    forecast_list = list()
     forecast_data = read_forecast()
     for data in forecast_data:
-        timestamp_list.append(data.timestamp)
-        temperature_list.append (data.temperature)
+        forecast_list.append(f'x: {data.timestamp}, y: {data.temperature}')
 
-    return render (request, 'chart.html', {'timestamp': timestamp_list, 'temperature': temperature_list})
+    return render (request, 'chart.html', {'forecast_data': forecast_list})
